e2a1794092f880e3f099b82e0742710e09cbcdcf
Tom <zuckm709@gmail.com>
2026-01-08 15:29:49 +0100

    shut typescript-up for a while

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Play, Pause, Zap, Wind, Turtle } from "lucide-react";
 
 const App = () => {
